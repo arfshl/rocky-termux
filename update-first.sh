68e0e3a2f8e2498ccbf3d223ec85bdbecc21d6d3
arfshl <98870352+arfshl@users.noreply.github.com>
2022-11-04 20:59:48 +0700

    Update update-first.sh

--- update-first.sh
+++ update-first.sh
@@ -4,4 +4,4 @@ curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-Rocky-8-Testing https://dl.rockylinux.org/p
 curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-rockyinfra https://dl.rockylinux.org/pub/rocky/RPM-GPG-KEY-rockyinfra
 dnf upgrade
 rm /var/lib/rpm/.rpm.lock
-
+dnf clean all
