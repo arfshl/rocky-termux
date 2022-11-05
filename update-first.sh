c9f3fbaf79f80654f0260d8d2b68932bec08ecaf
arfshl <98870352+arfshl@users.noreply.github.com>
2022-11-06 01:11:16 +0700

    Delete update-first.sh

--- update-first.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-Rocky-8 https://dl.rockylinux.org/pub/rocky/RPM-GPG-KEY-Rocky-8 
-curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-Rocky-8-Testing https://dl.rockylinux.org/pub/rocky/RPM-GPG-KEY-Rocky-8-Testing 
-curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-rockyinfra https://dl.rockylinux.org/pub/rocky/RPM-GPG-KEY-rockyinfra
-dnf upgrade
-rm /var/lib/rpm/.rpm.lock
-dnf clean all
