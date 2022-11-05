595b34b22038e59d03d091ec9e4a882ff01ad4bf
arfshl <98870352+arfshl@users.noreply.github.com>
2022-11-05 10:07:45 +0700

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,64 @@
+bin/sh
+# This script is modification of https://raw.githubusercontent.com/EXALAB/Anlinux-Resources/master/Scripts/DesktopEnvironment/Heavy/KDE/Ubuntu/de-ubuntu-kde.sh (Licensed under GPL-2.0)
+echo 'Installing KDE, please wait...'
+dnf groupinstall "KDE Plasma Workspaces" --skip-broken
+dnf install tigervnc-server dbus-x11 pulseaudio tigervnc-server-module -y
+echo 'Setting up KDE, please wait...'
+mkdir ~/.vnc
+
+echo '#!/bin/bash
+xrdb $HOME/.Xresources
+dbus-launch startplasma-x11
+export PULSE_SERVER=127.0.0.1' >> ~/.vnc/xstartup
+
+echo '#!/bin/sh
+export USER=root
+export HOME=/root
+
+case `uname -m` in
+	aarch64)
+		archurl="aarch64" ;;
+	arm*)
+		archurl="arm" ;;
+	amd64)
+		archurl="x86_64" ;;
+	x86_64)
+		archurl="x86_64" ;;	
+	i*86)
+		archurl="i386" ;;
+	x86)
+		archurl="i386" ;;
+	*)
+		echo "unknown architecture"; exit 1 ;;
+	esac
+
+LD_PRELOAD=/lib/${archurl}-linux-gnu/libgcc_s.so.1 vncserver -name remote-desktop -localhost no :1
+echo 'VNC server address: 127.0.0.1:1'' >> /usr/local/bin/start
+
+echo '#!/bin/sh
+export USER=root
+export HOME=/root
+
+vncserver -kill :1
+rm -rf /tmp/.X1-lock
+rm -rf /tmp/.X11-unix/X1' >> /usr/local/bin/stop
+
+echo '#!/bin/sh
+stop
+start' >> /usr/local/bin/restart
+
+cd /usr/local/bin
+chmod +x start
+chmod +x stop
+chmod +x restart
+cd
+chmod +x ~/.vnc/xstartup
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+dnf remove konqueror ksysguard plasma-discover kde-partition-manager kde-connect kwalletmanager -y
+passwd
+echo 'Starting up VNC Server'
+echo 'To start VNC server use start command'
+echo 'To stop VNC server use stop command'
+echo 'To restart VNC server use restart command'
+start
