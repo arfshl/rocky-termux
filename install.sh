aea2f4584dee6209b6a09d25ff091992f3e569bc
Alif <98870352+arfshl@users.noreply.github.com>
2024-11-19 11:18:38 +0700

    Update rootfs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'DISTRO_NAME="Rocky Linux"
-TARBALL_URL['aarch64']="https://raw.githubusercontent.com/rocky-linux/sig-cloud-instance-images/Rocky-9.4.20240523-Base-aarch64/layer.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/rocky-linux/sig-cloud-instance-images/raw/refs/heads/Rocky-9.4.20240523-Base-aarch64/layer.tar.xz"
 TARBALL_SHA256['aarch64']="ccc9fbef24be4d64e6430f4f5738a4c9f3ca6994ba174647fddec1f3e2714b54"' >> /$PREFIX/etc/proot-distro/rocky.sh
 
 chmod +x $PREFIX/etc/proot-distro/rocky.sh
